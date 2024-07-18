360bb2462aed74353983c203f8d2d58b3855ad92
muzu-85 <muzukondeth85@gmail.com>
2024-07-18 20:18:42 +0530

    lsgd id fix

--- src/app/user-register/page.tsx
+++ src/app/user-register/page.tsx
@@ -189,7 +189,7 @@ function UserRegisterForm() {
       districtId           : districts.find((item) => item.dis_name === values.district)?.dis_id.toString(),
       userPhoto            : '',
       wardNo               : parseInt(values.ward) || 0,
-      lsg                  : values.lsg
+      lsgd                 : lsgd.find((item) => item.lsg_name === values.lsg)?.lsg_id,
     };
 
     try {
